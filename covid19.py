ccfb1dd3761ae2c20619cf27c6ef49a28dc246cf
Ann311 <67332880+Ann311@users.noreply.github.com>
2020-07-13 09:26:52 +0530

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -2,7 +2,7 @@
 import pandas as pd
 data=pd.read_csv('covid19.csv')
 #Cleaning of the data
-data.drop(['Sno','Time','Date','ConfirmedIndianNational','ConfirmedForeignNational'],inplace=True,axis=1)
+data.drop(['Sno','Time','Date','ConfirmedIndianNational','ConfirmedForeignNational'],inplace=True,axis=1) #Here we cancel the mutiple column by a list
 #Splitting of data into x and y
 x=data.iloc[:,0].values
 y=data.iloc[:,1:].values
@@ -12,7 +12,7 @@ lab=LabelEncoder()
 x=lab.fit_transform(x)                   #Encoded the name of states into number in alphabetical order
 #Visualizing the data
 from matplotlib import pyplot as plt
-w=0.35                                   #variable which helps to visualize the graph more easily
+w=0.35                                   #variable is used as width of the bar
 plt.subplots(figsize=(10,7))
 p1=plt.bar(x,y[:,2],w,color='r')
 p2=plt.bar(x-w,y[:,0],w,color='g')
